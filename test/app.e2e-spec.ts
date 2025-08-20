635b09ae01c1c5cf04e7b93c1653fb00f3726b46
nitindd2018 <45159903+nitindd2018@users.noreply.github.com>
2025-08-20 18:11:16 +0530

    Update app.e2e-spec.ts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,6 +5,7 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let useless: false;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
